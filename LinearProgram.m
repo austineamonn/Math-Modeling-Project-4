78749be6ec4f67abb83441b5ce57c031f9fd8caf
Austin Nicolas <107045458+austineamonn@users.noreply.github.com>
2024-05-02 09:22:54 -0500

    added outletPercentage and functionality to set ax1,bx1,cx1

--- LinearProgram.m
+++ LinearProgram.m
@@ -1,4 +1,4 @@
-function [x, fval, exitf, lag, output]=LinearProgram(question,algorithm,salesLevel,velvet,priceIncrease,outlets,boundIncrease)
+function [x, fval, exitf, lag, output]=LinearProgram(question,algorithm,salesLevel,velvet,priceIncrease,outlets,boundIncrease,outletPercentage,ax1,bx1,cx1)
 
 %%Question
 
@@ -15,7 +15,8 @@ function [x, fval, exitf, lag, output]=LinearProgram(question,algorithm,salesLev
 
 %%SalesLevel
 
-%SalesLevel==0: 100,0,0 Full Sale scenario
+%SalesLevel==-1 no values given must fill in ax1,bx1 and cx1
+%SalesLevel==0: 100,0,0 perfect (full sale) scenario
 %SalesLevel==1: 85,10,5 great scenario
 %SalesLevel==2: 70,20,10 good scenario
 %SalesLevel==3: 35,45,20 ok scenario
@@ -28,8 +29,8 @@ function [x, fval, exitf, lag, output]=LinearProgram(question,algorithm,salesLev
 
 %%priceIncrease
 
-%increase prices for velvet garments by this percentage
-%decrease upper bounds for velvet garment production by this percentage
+%increase prices for silk garments by this percentage
+%decrease upper bounds for silk garment production by this percentage
 
 %%outlets
 
@@ -37,35 +38,56 @@ function [x, fval, exitf, lag, output]=LinearProgram(question,algorithm,salesLev
 %to be sold at 60% prices. If they are not sold there then they get thrown
 %out.
 %false = Fasion Star does not use outlets, all unsold products are thrown
-%out. Essentially b=0 and whatever b was is added to c.
+%out.
 
 %%boundIncrease
 
 %increase lower bounds for silk blouse and silk camisole production by this amount
 
+%%outletPercentage
+
+%if outlets==false then take outletPercentage % from b and add that to a.
+%The rest gets added to c.
+
+%%ax1,bx1,cx1
+
+%these inputs are non needed and will be filled in automatically if you
+%select a salesLevel
+%ax1 = amount of product sold at full price at main store
+%bx1 = amount of product sold at 60% price at outlet store
+%cx1 = amount of unsold product
+
 %%Set Up Linear Program
 
 %add in inputs in case inputs are not given
-if nargin<7
-    boundIncrease=0; %don't increase bound
-    if nargin<6
-        outlets=true; %assume using outlets
-        if nargin<5
-            priceIncrease=0; %do not increase prices
-            if nargin<4
-                velvet=false; %keep velvet equal to the other products
-                if nargin<3
-                    salesLevel=0; %use best case scenario levels
-                    if nargin<2
-                        algorithm='dual-simplex'; %set to dual simplex bc it's the default for linprog
-                        if nargin<1
-                            question=0; %set to 'base' algorithm
+if nargin<9
+    noSalesLevel=true; %no inputs for ax1,bx1, and cx1 are given
+    if nargin<8
+        outletPercentage=0.5; %assume half of outlets end up sold at full price
+        if nargin<7
+            boundIncrease=0; %don't increase bound
+            if nargin<6
+                outlets=true; %assume using outlets
+                if nargin<5
+                    priceIncrease=0; %do not increase prices
+                    if nargin<4
+                        velvet=false; %keep velvet equal to the other products
+                        if nargin<3
+                            salesLevel=0; %use best case scenario levels
+                            if nargin<2
+                                algorithm='dual-simplex'; %set to dual simplex bc it's the default for linprog
+                                if nargin<1
+                                    question=0; %set to 'base' algorithm
+                                end
+                            end
                         end
                     end
                 end
             end
         end
     end
+else
+    noSalesLevel=false; %inputs for ax1,bx1, and cx1 are given
 end
 
 if question==0 || question==2
@@ -915,9 +937,9 @@ elseif question==5
 
         if ~outlets
 
-            ax1=.1;
+            ax1=.15;
             bx1=0;
-            cx1=.9;
+            cx1=.85;
 
         elseif outlets
 
@@ -1012,103 +1034,83 @@ elseif question==6
         0;
         0];
 
-    if salesLevel==0
+    if salesLevel==-1
+
+        %use ax1,bx1,cx1 that were assigned
 
         if ~outlets
 
-            ax1=1;
+            ax1=ax1+(bx1*outletPercentage);
             bx1=0;
-            cx1=0;
-
-        elseif outlets
-
-            ax1=1;
-            bx1=0;
-            cx1=0;
-
-        else
-
-            error('invalid outlet input')
+            cx1=1-ax1;
 
         end
 
+        if noSalesLevel
+
+            error('You did not input ax1, bx1 or cx1. Use a different salesLevel or input these values.')
+
+        end
+
+    elseif salesLevel==0
+
+        ax1=1; %there is no no outlet scenario since all products are assumed to be sold at full price
+        bx1=0;
+        cx1=0;
+
     elseif salesLevel==1
+
+        ax1=.85;
+        bx1=.1;
+        cx1=.05;
 
         if ~outlets
 
-            ax1=.85;
+            ax1=ax1+(bx1*outletPercentage);
             bx1=0;
-            cx1=.15;
-
-        elseif outlets
-
-            ax1=.85;
-            bx1=.1;
-            cx1=.05;
-
-        else
-
-            error('invalid outlet input')
+            cx1=1-ax1;
 
         end
 
     elseif salesLevel==2
 
+        ax1=.7;
+        bx1=.2;
+        cx1=.1;
+
         if ~outlets
 
-            ax1=.7;
+            ax1=ax1+(bx1*outletPercentage);
             bx1=0;
-            cx1=.3;
-
-        elseif outlets
-
-            ax1=.7;
-            bx1=.2;
-            cx1=.1;
-
-        else
-
-            error('invalid outlet input')
+            cx1=1-ax1;
 
         end
 
     elseif salesLevel==3
 
+        ax1=.35;
+        bx1=.45;
+        cx1=.2;
+
         if ~outlets
 
-            ax1=.35;
+            ax1=ax1+(bx1*outletPercentage);
             bx1=0;
-            cx1=.65;
-
-        elseif outlets
-
-            ax1=.35;
-            bx1=.45;
-            cx1=.2;
-
-        else
-
-            error('invalid outlet input')
+            cx1=1-ax1;
 
         end
 
     elseif salesLevel==4
 
+        ax1=.1;
+        bx1=.3;
+        cx1=.6;
+
         if ~outlets
 
-            ax1=.1;
+            ax1=ax1+(bx1*outletPercentage);
             bx1=0;
-            cx1=.9;
-
-        elseif outlets
-
-            ax1=.1;
-            bx1=.3;
-            cx1=.6;
-
-        else
-
-            error('invalid outlet input')
+            cx1=1-ax1;
 
         end
 
@@ -1136,8 +1138,8 @@ elseif question==6
 
     c=[profitCalculator(ax1,bx1,cx1,300,190,0),
         profitCalculator(ax1,bx1,cx1,450,240,0),
-        profitCalculator(ax1,bx1,cx1,180,119.5,priceIncrease), %
-        profitCalculator(ax1,bx1,cx1,120,66.5,priceIncrease),%
+        profitCalculator(ax1,bx1,cx1,180,119.5,priceIncrease), 
+        profitCalculator(ax1,bx1,cx1,120,66.5,priceIncrease),
         profitCalculator(ax1,bx1,cx1,270,126.75,0),
         profitCalculator(ax1,bx1,cx1,320,164.75,0),
         profitCalculator(ax1,bx7,cx7,350,214,0), %Velvet Pants
