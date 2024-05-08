1d239172d86c56a2395017836fc990cc254ca2e9
hallsam003 <160756157+hallsam003@users.noreply.github.com>
2024-05-08 08:29:45 -0500

    Update fashionLoop.m

--- fashionLoop.m
+++ fashionLoop.m
@@ -1,4 +1,4 @@
-function fashionLoop(loop,salesLevel,velvet,outlets)
+function fashionLoop(loop,salesLevel,velvet,outlets, gnumb)
     
     switch nargin
         case 1
@@ -12,10 +12,13 @@ function fashionLoop(loop,salesLevel,velvet,outlets)
 % loop = 0: no loop
 % loop = 1: loop over price increase
 % loop = 2: loop over lower bound for x3 and x4
-% loop = 3: make outlet true and loop over how th eprecnese of outlets
+% loop = 3: make outlet true and loop over how the precnese of outlets
 % reflects the decreased demand in a.
 % loop = 4: loop over outlet price (how much more sold in a results from no
 % outlets)
+% loop = 5: loop over profit increase for velvet shirts for question 1
+% loop = 6: loop over material supply gnumb: (1-7)
+% loop = 7: loop over increased, gnumn (1-11)
 
 
      A=[3,0,0,0,0,2.5,0,0,0,0,0;
@@ -32,13 +35,13 @@ function fashionLoop(loop,salesLevel,velvet,outlets)
         4000;
         12000;
         15000;
-        Inf; %because no upper bound
-        Inf;
+        20000; %because no upper bound
+        20000;
         5500;
-        Inf;
-        Inf;
+        20000;
+        20000;
         6000;
-        Inf];
+        20000];
 
     l=[4200;
         0;
@@ -47,7 +50,7 @@ function fashionLoop(loop,salesLevel,velvet,outlets)
         2800;
         3000;
         0;
-        200; %increase so some get produced
+        0; %increase so some get produced
         0;
         0;
         0];
@@ -215,13 +218,13 @@ options = optimoptions('linprog','Algorithm','dual-simplex');
                   4000;
                   12000*(1-priceIncvec(i));
                   15000*(1-priceIncvec(i));
-                  Inf; %because no upper bound
-                  Inf;
+                  20000; %because no upper bound
+                  20000;
                   5500;
-                  Inf;
-                  Inf;
+                  20000;
+                  20000;
                   6000;
-                  Inf];
+                  20000];
 
                  c=[profitCalculator(ax1,bx1,cx1,300,190,0),
                   profitCalculator(ax1,bx1,cx1,450,240,0),
@@ -236,13 +239,14 @@ options = optimoptions('linprog','Algorithm','dual-simplex');
                   profitCalculator(ax1,bx1,cx1,120,93.3750,0)];
 
               [~, fval] = linprog(c, A, b, [] , [] , l, u, options);
-              profitVec(i) = -1*fval;
+              profitVec(i) = -1*fval - 2060000;
 
             end
 
                figure;
                  hold on
-                  plot(priceIncvec, profitVec, 'LineStyle','-')
+                  plot(priceIncvec, profitVec, 'LineWidth', 3.0, 'Color', [0.6 0 1])
+                
 
 
          elseif loop == 2
@@ -264,7 +268,7 @@ options = optimoptions('linprog','Algorithm','dual-simplex');
                        0];
 
               [~, fval] = linprog(c, A, b, [] , [] , l, u, options);
-              ProfitVec(i) = -1*fval;
+              ProfitVec(i) = -1*fval - 2060000;
 
                 end 
                     figure;
@@ -328,16 +332,18 @@ options = optimoptions('linprog','Algorithm','dual-simplex');
                 profitCalculator(ax1(i),bx1,cx1(i),120,93.3750,0)];
 
               [~, fval] = linprog(c, A, b, [] , [] , l, u, options);
-              ProfitVec(i) = -1*fval;
+              ProfitVec(i) = -1*fval - 2060000;
         end
             figure;
             hold on
-            plot(ax1, ProfitVec, 'LineStyle','-');
+            plot(ax1, ProfitVec, 'LineWidth', 3.0, 'Color', [0.6 0 1]);
+            xlabel('Proportion of garments being sold by Fashion Star');
+                    ylabel('Profit');
+                    title('Effects of Outlets on Fashion Star Sales');
 
 
         elseif loop == 4
-            outlets = false;
-            outletPriceVec = linspace(0:.75, 100);
+            outletPriceVec = linspace(0,.75, 100);
             ProfitVec = zeros(1, length(outletPriceVec));
 
             for i=1:length(outletPriceVec)
@@ -392,14 +398,137 @@ options = optimoptions('linprog','Algorithm','dual-simplex');
                 profitCalculator(ax1,bx1,cx1,120,93.3750,0)];
 
             [~, fval] = linprog(c, A, b, [] , [] , l, u, options);
-             ProfitVec(i) = -1*fval;
+             ProfitVec(i) = -1*fval - 2060000;
             end 
+            outletPriceVec
+            ProfitVec
             figure;
             hold on
-            plot(outletPriceVec, ProfitVec, 'LineStyle','-');
+            plot(outletPriceVec, ProfitVec, 'LineWidth', 3.0, 'Color', [0.6 0 1]);
+            xlabel('Outlet Percentage');
+                    ylabel('Profit');
+                    title('Effects of Removing Outlets on Fashion Star Sales');
+
+        elseif loop == 5      
+
+            priceIncreaseVec = linspace(0,.95, 100);
+            ProfitVec = zeros(1,length(priceIncreaseVec));
+                for i=1:length(priceIncreaseVec)
+
+                     c=[profitCalculator(ax1,bx1,cx1,300,190,0),
+                        profitCalculator(ax1,bx1,cx1,450,240,0),
+                        profitCalculator(ax1,bx1,cx1,180,119.5,0), %
+                        profitCalculator(ax1,bx1,cx1,120,66.5,0),%
+                        profitCalculator(ax1,bx1,cx1,270,126.75,0),
+                        profitCalculator(ax1,bx1,cx1,320,164.75,0),
+                        profitCalculator(ax1,bx7,cx7,350,214,0), %Velvet Pants
+                        profitCalculator(ax1,bx1,cx1,130,63.75,0),
+                        profitCalculator(ax1,bx1,cx1,75,41.25,0),
+                        profitCalculator(ax1,bx10,cx10,200,178,priceIncreaseVec(i)), %Velvet Shirt -- increase price for velvet only
+                        profitCalculator(ax1,bx1,cx1,120,93.3750,0)
+                        ];
+
+                     u=[20000;
+                        4000;
+                        12000;
+                        15000;
+                        20000; %because no upper bound
+                        20000;
+                        5500;
+                        20000;
+                        20000;
+                        6000*(1-priceIncreaseVec(i)); %Do not produce velvet pants
+                        20000];
+
+                     [~, fval] = linprog(c, A, b, [] , [] , l, u, options);
+                       ProfitVec(i) = -1*fval - 2060000;
+                end
+               figure;
+                hold on
+                plot(priceIncreaseVec, ProfitVec, 'LineStyle', '-', 'Color', [0.6 0 1]); % Bright purple
+                xlabel('Velvet shirt price increase');
+                ylabel('Profit');
+                title('Total Profits from Changing Velvet Shirt prices');
+
+             
+        elseif loop == 6
+            matVec = linspace(.75*b(gnumb), 3.2*b(gnumb), 250);
+            ProfitVec = zeros(1,length(matVec));
+            ogMat = b(gnumb);
+
+                for i=1:length(matVec)
+                    b(gnumb) = matVec(i);
+                    [~, fval] = linprog(c, A, b, [] , [] , l, u, options);
+                    ProfitVec(i) = -1*fval - 2060000;
+                end
+
+                figure;
+                hold on
+                plot(matVec, ProfitVec, 'LineWidth', 3.0, 'Color', [0.6 0 1]); % Bright purple
+                xline(ogMat, 'LineStyle', '--', 'Color', [0.6 0 1])
+                xlabel('Acetate Supply');
+                ylabel('Profit');
+                title('Total Profits from Changing Acetate Supply');
+
+        elseif loop == 7
+                currentCost = -1*c(8);
+                matVec = linspace(.75*-1*currentCost, 1.5*-1*currentCost, 250);
+               
+                currentCost2 = -1*c(9);
+                matVec2 = linspace(.75*-1*currentCost2, 1.5*-1*currentCost2, 250);
+
+               % currentCost3 = -1*c(6);
+               % matVec3 = linspace(.75*-1*currentCost3, 1.5*-1*currentCost3, 250);
+
+               % currentCost4 = -1*c(5);
+               % matVec4 = linspace(.75*-1*currentCost4, 1.5*-1*currentCost4, 250);
+
+                ProfitVec = zeros(1,length(matVec));
+
+                    for i=1:length(matVec)
+                  %  c(7) = profitCalculator(ax1,bx7,cx7,350,matVec(i),0);
+                  %  c(1) =  profitCalculator(ax1,bx1,cx1,300,matVec2(i),0);
+                  %  c(6) = profitCalculator(ax1,bx1,cx1,320,matVec3(i),0);
+                  %  c(5) = profitCalculator(ax1,bx1,cx1,270,matVec(i),0);
+                 %  c(2) = profitCalculator(ax1,bx1,cx1,450,matVec(i),0);
+                % c(3) = profitCalculator(ax1,bx1,cx1,180,matVec(i),0);
+                 %c(4) = profitCalculator(ax1,bx1,cx1,120,matVec2(i),0);
+                % c(11) = profitCalculator(ax1,bx1,cx1,120,matVec2(i),0);
+                c(8) = profitCalculator(ax1,bx1,cx1,130,matVec(i),0);
+                c(9) = profitCalculator(ax1,bx1,cx1,75,matVec2(i),0);
+
+
+                    [~, fval] = linprog(c, A, b, [] , [] , l, u, options);
+                    ProfitVec(i) = -1*fval - 2060000;
+                    end
+                    % Calculate the slope
+                    delta_profit = ProfitVec(end) - ProfitVec(1);
+                    delta_expenses = 150 - 75;
+                    slope = -(delta_profit / delta_expenses);
+
+                  
+
+                % Plot the graph
+                figure;
+                hold on;
+                plot(linspace(75, 150, 250), flip(ProfitVec), 'LineWidth', 3.0, 'Color', [0.6 0 1]); % Bright purple
+                xlabel('Percent of Standard Cotton Expenses');
+                ylabel('Profit');
+                title('Total Profits from Changing Cotton Expenses');
+
+
+        slope_text = ['Slope: ', num2str(slope)];
+            text(150, ProfitVec(end), slope_text, 'HorizontalAlignment', 'right', 'VerticalAlignment', 'bottom', 'FontSize', 18);
+            hold off;
+
+     
+
+   
+
+
     end 
 
-end 
+end
 
 
 function [ans]=profitCalculator(a,b,c,price,costs,priceIncrease)
