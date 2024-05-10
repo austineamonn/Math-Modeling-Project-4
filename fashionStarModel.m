ee5e9fd05389c119f3b82734cbd14274b6fa53f5
Austin Nicolas <107045458+austineamonn@users.noreply.github.com>
2024-05-10 11:20:48 -0500

    Update fashionStarModel.m
    added stuff for the supply increase function

--- fashionStarModel.m
+++ fashionStarModel.m
@@ -1,10 +1,24 @@
-function [x, fval] = fashionStarModel(question, salesLevel, outlets, priceIncrease)
+function [x, fval] = fashionStarModel(question, salesLevel, outlets, priceIncrease, material, supplyIncrease, ax1)
 
 % question 0: optimal sales level with no changes (outlets true or false)
 % question 1: increase velvet sales price
 % question 2: No velvet returns, price increase of shirt option
 % question 3: Wool blazer cost increase, price increase option
 % question 4: acetate supply increase
+
+%material: The place in the matrix for each material
+%ex: Wool Material == 2
+
+if nargin<7
+    ax1=.97; %fills in for salesLevel 1
+    if nargin<6
+        supplyIncrease=10000; %fills in for exact increase from question 4
+        if nargin<5
+            material=2; %fills in for Wool Material
+        end
+    end
+end
+
 
 A = [3,0,0,0,0,2.5,0,0,0,0,0;  % Wool
     2,0,0,1.5,1.5,2,0,0,0,0,0; % Acetate
@@ -84,6 +98,16 @@ elseif salesLevel==4
         bx1=(5/6)*(1-ax1);
         cx1=1-bx1-ax1;
     end
+else
+    if ~outlets
+        %ax1 is given
+        bx1=0;
+        cx1=1-ax1;
+    elseif outlets
+        %ax1 is given
+        bx1=(5/6)*(1-ax1);
+        cx1=1-bx1-ax1;
+    end
 end
     
 c = [profitCalculator(ax1,bx1,cx1,300,190,0),      ... Wool slacks
@@ -98,14 +122,14 @@ c = [profitCalculator(ax1,bx1,cx1,300,190,0),      ... Wool slacks
      profitCalculator(ax1,bx1,cx1,200,178,0),      ... Velvet Shirt
      profitCalculator(ax1,bx1,cx1,120,93.3750,0)]; ... Button-down blouse
 
-b = [45000; % Wool
-     28000; % Acetate
-     9000;  % Cashmere
-     18000; % Silk
-     30000; % Rayon
-     20000; % Velvet
-     30000; % Cotton
-     0;
+b = [45000; % Wool ... material == 1
+     28000; % Acetate ... material == 2
+     9000;  % Cashmere ... material == 3
+     18000; % Silk ... material == 4
+     30000; % Rayon ... material == 5
+     20000; % Velvet ... material == 6
+     30000; % Cotton ... material == 7
+     0; %scrap constraints
      0];
 
 % Increase velvet shirt price
@@ -126,9 +150,9 @@ b = [45000; % Wool
     elseif question == 3
         c(6) =  profitCalculator(ax1,bx1,cx1,320,244.75,priceIncrease); % Wool blazer
   
-        % acetate supply increase
+        % product supply increase
     elseif question == 4
-        b(2) = 38000; % new acetate material supply
+        b(material) = b(material)+supplyIncrease; % new material supply
     end 
    
 
@@ -139,6 +163,6 @@ options = optimoptions('linprog','Algorithm','dual-simplex');
 fval = (-1*fval) - 860000 - (3*1200000);
 end 
 
-function [ans]=profitCalculator(a,b,c,price,costs,priceIncrease)
-    ans=-(price*(1+priceIncrease)*(a+0.6*b+0*c)-costs);
+function [ans1]=profitCalculator(a,b,c,price,costs,priceIncrease)
+    ans1=-(price*(1+priceIncrease)*(a+0.6*b+0*c)-costs);
 end
